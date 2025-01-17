6771838b7b67ebed68626d6e98feb651f3604149
unknown <castro.caique@outlook.com>
2025-01-17 11:39:43 -0300

    format

--- calc.py
+++ calc.py
@@ -1,4 +1,7 @@
 from math import pow, sqrt
+print("="*20)
+print("Bem-vindo ao APP de calculadora")
+print("="*20)
 
 escolha = str(input("""Escolha sua opção 
                     (+, -, *, /, **, //)\n"""))
@@ -35,7 +38,8 @@ elif escolha == "/":
 elif escolha == "**":
    n1 = float(input("Insira o primeiro número:"))
    n2 = float(input("Insira o segundo número:",))
-   print(resultado = float(pow)(n1,n2) )
+   resultado = float(pow(n1,n2))
+   print(resultado)
 
 elif escolha == "//":
    n1 = float(input("Insira o número:"))
